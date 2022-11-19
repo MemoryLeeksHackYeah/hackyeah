1b75d52999dd86f49033e05bb079cfa56bdd8689
Mateusz Kocik <mateusz.kocik9dev@gmail.com>
2022-11-19 18:15:40 +0100

    Remove login register

--- source/hackyeah_ml/app/models.py
+++ source/hackyeah_ml/app/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class User(AbstractUser):
-    def __str__(self):
-       return f"{self.username}"
 
 class RemoteHub(models.Model):
     id = models.IntegerField(primary_key=True)
--- source/hackyeah_ml/app/urls.py
+++ source/hackyeah_ml/app/urls.py
@@ -5,9 +5,6 @@ from . import views
 urlpatterns = [
     #Main paths
     path('', views.index, name='index'),
-    path("login", views.login_view, name="login"),
-    path("logout", views.logout_view, name="logout"),
-    path("register", views.register, name="register"),
 
     #Subtabs
     path('fleet', views.fleet, name="fleet"),
