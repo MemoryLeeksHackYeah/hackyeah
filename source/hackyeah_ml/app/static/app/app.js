862379bb6c2ff15d745536570d52faa21e61c279
Jakub Łata <lootjakub@gmail.com>
2022-11-20 01:23:48 +0100

    Added buttons in index view

--- source/hackyeah_ml/app/static/app/app.js
+++ source/hackyeah_ml/app/static/app/app.js
@@ -1,8 +1,10 @@
 // Initialize and add the map
 function initMap() {
-    // The location of Uluru
-    const krakow = { lat: 50.064, lng: 19.945 };
-    // The map, centered at Uluru
+
+    // The location of Krakow
+    const krakow = { lat: 50.060, lng: 19.948 };
+    const mama = { lat: 50.066, lng: 19.950 };
+    // The map, centered at Krakow
     const map = new google.maps.Map(document.getElementById("map"), {
       zoom: 12,
       center: krakow,
@@ -12,6 +14,14 @@ function initMap() {
       position: krakow,
       map: map,
     });
+    // The marker, positioned at Krakow
+    const marker2 = new google.maps.Marker({
+        position: mama,
+        map: map,
+        });
+
   }
   
   window.initMap = initMap;
+
+  
